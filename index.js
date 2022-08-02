f1354365ece1935f2e9f2d2ae2cacd67a5702e34
Luqman <luqmanxcz@gmail.com>
2022-08-01 23:45:04 -0400

    create the unordered list

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+console.log(ulEl)
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value)
