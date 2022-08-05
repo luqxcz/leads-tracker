086c9d58c3f02fdce73c76e0fdc238baee4920d4
Luqman <luqmanxcz@gmail.com>
2022-08-04 20:57:38 -0400

    Save the leads to localStorage

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value)
-
   inputEl.value = ""
+
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
   renderLeads()
+
+  // To verify that it works
+  console.log(localStorage.getItem("myLeads"))
 })
 
 
 function renderLeads() {
   let listItems = ""
-
   for (let i = 0; i < myLeads.length; i++) {
-    // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-
     listItems += `
         <li>
           <a target='_blank' href='${myLeads[i]}'>
